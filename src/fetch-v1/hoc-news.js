da0508d30b80df63510589f1cab33055e5179fc5
ambarkiy <ambarki.youssef@gmail.com>
2019-12-11 09:02:42 +0100

    recherche serveur, pagination, nombre d'élément par page

--- src/fetch-v1/hoc-news.js
+++ src/fetch-v1/hoc-news.js
@@ -1,47 +1,71 @@
 import React, { Component } from 'react';
 import URL_HELPER from '../CONSTANTS';
 import TableView from './table-view';
+import SearchBar from './search-bar';
 
 class HocNews extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { result: null, searchTerm: URL_HELPER.DEFAULT_QUERY };
+		this.fetchHits = this.fetchHits.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
 		this.setSearchResult = this.setSearchResult.bind(this);
 		this.onDissmiss = this.onDissmiss.bind(this);
+		this.onSubmitSearch = this.onSubmitSearch.bind(this);
 	}
+	fetchHits = (searchTerm, page = '0', hitsParPage = URL_HELPER.DEFAULT_HITS_PER_PAGE) => {
+		const urlRequest = `${URL_HELPER.PATH_BASE}${URL_HELPER.PATH_SEARCH}?${URL_HELPER.PARAM_SEARCH}
+		${searchTerm}&${URL_HELPER.PARAM_PAGE}${page}&${URL_HELPER.PARAM_HITS_PER_PAGE}${hitsParPage}`;
+		fetch(urlRequest)
+			.then((response) => response.json())
+			.then((result) => this.setSearchResult(result))
+			.catch((error) => error);
+	};
 
 	componentDidMount() {
 		const { searchTerm } = this.state;
-		const urlRequest = `${URL_HELPER.PATH_BASE}${URL_HELPER.PATH_SEARCH}?${URL_HELPER.PARAM_SEARCH}${searchTerm}`;
-		fetch(urlRequest)
-			.then((response) => {
-				// étape obligatoire dans l'utilisation de fetch sinon il s'agit d'un stream
-				let jsonResponse = response.json();
-				return jsonResponse;
-			})
-			.then((result) => {
-				this.setSearchResult(result);
-			})
-			.catch((error) => {
-				return error;
-			});
+		this.fetchHits(searchTerm);
 	}
 
 	render() {
 		const { searchTerm, result } = this.state;
-		if (!result) return null;
+		const page = (result && result.page) || 0;
+
 		return (
 			<div>
 				<h4>V1 recupération des news</h4>
-
-				<TableView list={result.hits} onDissmiss={this.onDissmiss} />
+				<SearchBar value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSubmitSearch}>
+					Rechercher
+				</SearchBar>
+				{result && <TableView list={result.hits} onDissmiss={this.onDissmiss} />}
+				<button onClick={() => this.fetchHits(searchTerm, page + 1)}>Suivant</button>
 			</div>
 		);
 	}
 
-	setSearchResult = (result) => this.setState({ result });
-	onSearchChange = (e) => this.setState({ searchTerm: e.target.value });
+	onSearchChange = (e) => {
+		const searchTerm = e.target.value;
+		this.setState({ searchTerm: searchTerm });
+	};
+
+	onSubmitSearch = (event) => {
+		const { searchTerm } = this.state;
+		this.fetchHits(searchTerm);
+		event.preventDefault();
+	};
+
+	setSearchResult = (result) => {
+		const { hits, page } = result;
+		const oldHits = page !== 0 ? this.state.result.hits : [];
+		const updatedHits = [
+			...oldHits,
+			...hits
+		];
+		this.setState({
+			result: { hits: updatedHits, page }
+		});
+	};
+
 	onDissmiss = (id) => {
 		const isNotId = (item) => item.objectID !== id;
 		const updatedHits = this.state.result.hits.filter(isNotId);
