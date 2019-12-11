1e226e38a06450cad66c30563d2a18cf5fda9494
ambarkiy <ambarki.youssef@gmail.com>
2019-12-11 06:46:27 +0100

    bind de la fonction de suppression, mis à jour du resultat à l'aide de l'operateur spread pour les objets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import { URL_TEST } from './CONSTANTS.js';
+import HocNews from './fetch-v1/hoc-news';
 
 class App extends Component {
 	render() {
-		return <pre>{URL_TEST}</pre>;
+		return (
+			<div>
+				<pre>{URL_TEST}</pre>
+				<div>
+					<HocNews />
+				</div>
+			</div>
+		);
 	}
 }
 
--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,6 +1,9 @@
-const DEFAULT_QUERY = 'redux',
-	PATH_BASE = 'https://hn.algolia.com/api/v1',
-	PATH_SEARCH = '/search',
-	PARAM_SEARCH = 'query=';
+const URL_HELPER = {
+	DEFAULT_QUERY: 'redux',
+	PATH_BASE: 'https://hn.algolia.com/api/v1',
+	PATH_SEARCH: '/search',
+	PARAM_SEARCH: 'query='
+};
+export default URL_HELPER;
 
-export const URL_TEST = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+export const URL_TEST = `${URL_HELPER.PATH_BASE}${URL_HELPER.PATH_SEARCH}?${URL_HELPER.PARAM_SEARCH}${URL_HELPER.DEFAULT_QUERY}`;
